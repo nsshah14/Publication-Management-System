daed009d20e7905a49ff11d38bc163b83ebbc46b
Parth <pthosan@ncsu.edu>
2022-04-07 19:20:50 -0400

    Added Chapters in Books (Insert)

--- Main/src/operations/Production.java
+++ Main/src/operations/Production.java
@@ -60,7 +60,6 @@ public class Production {
                 String getpublicationID = "SELECT PublicationID FROM Publication ORDER BY PublicationID DESC LIMIT 1";
                 PreparedStatement getValueofID=conn.prepareStatement(getpublicationID);
                 ResultSet publicationID =  getValueofID.executeQuery();
-                System.out.println(publicationID);
                 publicationID.first();
                 int PubID=publicationID.getInt("PublicationID");
                 System.out.println(PubID);
@@ -77,11 +76,33 @@ public class Production {
                 insertIntoBook.executeQuery();
 
                 System.out.println("Inserted into Books Table");
-            
+                
+                System.out.println("\t\t [1] Enter the Chapter Information in a Book:");
+                System.out.println("\t\t Any other key to return to main menu");
+                System.out.print("Input Command: ");
+                userInput = inputRead.next();
+
+                if(userInput.equals("1"))
+                {
+                    System.out.println("Enter the Chapter ID:");
+                    int chapID=inputRead.nextInt();  
+                    System.out.println("Enter the Number of Pages Chapters of Book:");
+                    int numOfPages=inputRead.nextInt();  
+                    String pages_query = "INSERT INTO Chapters (PublicationID,ChapterID,Number_of_pages) VALUES(?,?,?);";
+                    PreparedStatement insertPagesIntoBook=conn.prepareStatement(pages_query);
+                    insertPagesIntoBook.setInt(1, PubID);
+                    insertPagesIntoBook.setInt(2, chapID);
+                    insertPagesIntoBook.setInt(3, numOfPages);
+                    insertPagesIntoBook.executeQuery();
+                    System.out.println("Inserted into Chapters Table");
+                }
+                else{
+                    return true;
+                }
             }
             else if(userInput.equals("2"))
             {
-
+                
             }
             else{
                 System.out.println("Wrong Input Given");
