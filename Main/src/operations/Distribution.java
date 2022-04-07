2393d589dfce371e86869b68b6a1eebc95e5ce55
freakNewton <shahnisarg9696@gmail.com>
2022-04-07 19:11:54 -0400

    updated distribution

--- Main/src/operations/Distribution.java
+++ Main/src/operations/Distribution.java
@@ -73,20 +73,49 @@ public class Distribution {
          return true;
       }
 
-      boolean updateDist(Connection conn, Scanner inputreader){
+      public static boolean updateDist(Connection conn, Scanner inputreader){
 
-         // String condcolname, String updatecolname, Object cond, Object newval
+         List<String> condcolnames=new ArrayList();
+          List<Object> condcolvals=new ArrayList();
 
-         try{
-            PreparedStatement stupdate=conn.prepareStatement("UPDATE Distributor SET "+updatecolname+"=? WHERE "+condcolname+"=?;");
-            
-            //todo
+          System.out.println("Enter number of conditions:");
+          int n=inputreader.nextInt();
+          for(int i=0;i<n;i++){
+             System.out.println("Enter Column name:");
+             condcolnames.add(inputreader.next());
+             System.out.println("Enter column value:");
+             condcolvals.add(inputreader.next());
+          }
 
-         } catch(Exception e){
+          System.out.println("Enter Column Name to update:");
+          String toUpdateCol=inputreader.next();
 
-            return false;
-         }
-         return true;
+          System.out.println("Enter new value:");
+          String toUpdateVal=inputreader.next();
+
+
+          try{
+             StringBuilder query=new StringBuilder("UPDATE Distributor SET "+toUpdateCol+"=? where ");
+             for(int i=0; i<condcolnames.size();i++){
+                query.append(condcolnames.get(i) + "=? and ");
+             }
+             query.replace(query.length()-5,query.length()-1,";");
+
+            //  System.out.println(query.toString());
+             PreparedStatement stupdate=conn.prepareStatement(query.toString());
+             stupdate.setObject(1,toUpdateVal);
+             for(int i=0; i<condcolnames.size();i++){
+                stupdate.setObject(i+2,condcolvals.get(i));
+             }
+
+             stupdate.executeQuery();
+
+             System.out.println("Rows Updated");
+          } catch(Exception e){
+             e.printStackTrace();
+             return false;
+          }
+          return true;
       }
 
       public static boolean deleteDist(Connection conn, Scanner inputreader){
@@ -107,15 +136,16 @@ public class Distribution {
             for(int i=0; i<delcolnames.size();i++){
                query.append(delcolnames.get(i) + "=? and ");
             }
-            query.replace(query.length()-6,query.length()-1,";");
-            // PreparedStatement stdelete=conn.prepareStatement(query);
+            query.replace(query.length()-5,query.length()-1,";");
+            PreparedStatement stdelete=conn.prepareStatement(query.toString());
             
-            // for(int i=0;i<delcolvals.size();i++)
-            //    stdelete.setObject(i, delcolvals.get(i));
+            for(int i=0;i<delcolvals.size();i++)
+               stdelete.setObject(i+1, delcolvals.get(i));
 
-            System.out.println(query.toString());
-            // stdelete.executeQuery();
+            // System.out.println(query.toString());
+            stdelete.executeQuery();
 
+            System.out.println("Rows Deleted");
          }catch(Exception e){
             e.printStackTrace();
             return false;
