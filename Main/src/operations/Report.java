9137a3265083721f2f75c103a2189481e42d3973
Prashan Sengo <psengo@ncsu.edu>
2022-04-08 14:19:46 -0400

    worked on method 21

--- Main/src/operations/Report.java
+++ Main/src/operations/Report.java
@@ -48,9 +48,9 @@ public class Report {
             		+ "(SELECT PlacesOrder.OrderID FROM PlacesOrder "
             		+ "JOIN TransactionDetails ON PlacesOrder.OrderID = TransactionDetails.OrderID "
             		+ "JOIN Consist ON Consist.OrderID = TransactionDetails.OrderID "
-            		+ "WHERE Consist.PublicationID = 1 "
-            		+ "AND PlacesOrder.DistributorID = 2 "
-            		+ "AND MONTH(TransactionDetails.DeliveryDate)= 1)", publicationID, distributorID, month);
+            		+ "WHERE Consist.PublicationID = %s "
+            		+ "AND PlacesOrder.DistributorID = %s "
+            		+ "AND MONTH(TransactionDetails.DeliveryDate)= %s)", publicationID, distributorID, month);
             
             ResultSet rs = stmt.executeQuery(inputStatement);
             while(rs.next()){
@@ -61,28 +61,52 @@ public class Report {
             System.out.println("Ok");
         }catch(SQLException e){
             e.printStackTrace();
+            return false; 
         }
         return true;
 	}
 	
 	//System.out.println("\t\t [21] - Generate montly reports: total revenue of the publishing house");
 	
-	public static void monthlyRevenue(Connection con, Scanner scan ){
+	public static boolean monthlyRevenue(Connection con, Scanner scan ){
 
-		//TODO: count distributor ids. 
+		//get the month
+        System.out.print("Enter Month number: "); 
+        String month = scan.nextLine();
+
+		//orders, places orders, consists, transactionDetails 
+        //TODO: merge places orders,transactionDetails and consists by orderID. 
+        //TODO: extract all orderids by specified  month of transaction.
+        //TODO: aggregate the price  of the orderids. 
         try(Statement stmt = con.createStatement()){
-            //String inputStatement = String.format("INSERT into Publication VALUES(%s, '%s', '%s', '%s', '%s')", id, title, date, topics, periodicity);
-            //stmt.executeUpdate(inputStatement);
+        	ArrayList<Integer> OrderIDs = new ArrayList<Integer>();
+        	
+        	
+        	//"SELECT SUM(Price), SUM(NumCopies) FROM Orders WHERE Orders.OrderID IN (SELECT PlacesOrder.OrderID FROM PlacesOrder JOIN TransactionDetails ON PlacesOrder.OrderID = TransactionDetails.OrderID JOIN Consist ON Consist.OrderID = TransactionDetails.OrderID WHERE Consist.PublicationID = 1 AND PlacesOrder.DistributorID = 2 AND MONTH(TransactionDetails.DeliveryDate)= 1);"
+        	
+            String inputStatement = String.format("SELECT SUM(Price) FROM Orders "
+            		+ "WHERE Orders.OrderID IN "
+            		+ "(SELECT PlacesOrder.OrderID FROM PlacesOrder "
+            		+ "JOIN TransactionDetails ON PlacesOrder.OrderID = TransactionDetails.OrderID "
+            		+ "JOIN Consist ON Consist.OrderID = TransactionDetails.OrderID "
+            		+ "WHERE MONTH(TransactionDetails.DeliveryDate)= %s)", month);
             
+            ResultSet rs = stmt.executeQuery(inputStatement);
+            while(rs.next()){
+                //grab ids
+                System.out.println("SUM MONTHLY Price: " + rs.getString(1));
+             }
             System.out.println("Ok");
         }catch(SQLException e){
             e.printStackTrace();
+            return false; 
         }
+        return true;
 	}
 	
 	
 	//System.out.println("\t\t [22] - Generate montly reports: total expenses (i.e., shipping costs and salaries)");
-	public static void monthlyExpense(Connection con, Scanner scan ){
+	public static boolean monthlyExpense(Connection con, Scanner scan ){
 
 		//TODO: count distributor ids. 
         try(Statement stmt = con.createStatement()){
@@ -93,10 +117,11 @@ public class Report {
         }catch(SQLException e){
             e.printStackTrace();
         }
+        return true;
 	}
 	
 	//System.out.println("\t\t [23] - Calculate the total current number of distributors");
-	public static void totalDistributor(Connection con, Scanner scan ){
+	public static boolean totalDistributor(Connection con, Scanner scan ){
 
 		//TODO: count distributor ids. 
         try(Statement stmt = con.createStatement()){
@@ -107,10 +132,12 @@ public class Report {
         }catch(SQLException e){
             e.printStackTrace();
         }
+        return true;
+        
 	}
 	
 	//System.out.println("\t\t [24] - Calculate total revenue (since inception) per city, per distributor, and per location");
-	public static void totalRevenue(Connection con, Scanner scan ){
+	public static boolean totalRevenue(Connection con, Scanner scan ){
 
 		//TODO: count distributor ids. 
         try(Statement stmt = con.createStatement()){
@@ -121,10 +148,11 @@ public class Report {
         }catch(SQLException e){
             e.printStackTrace();
         }
+        return true;
 	}
 	
 	//System.out.println("\t\t [25] - Calculate total payments to the editors and authors, per time period and per work type (book authorship, article authorship, or editorial work)");
-	public static void totalPayment(Connection con, Scanner scan ){
+	public static boolean totalPayment(Connection con, Scanner scan ){
 
 		//TODO: count distributor ids. 
         try(Statement stmt = con.createStatement()){
@@ -135,5 +163,6 @@ public class Report {
         }catch(SQLException e){
             e.printStackTrace();
         }
+        return true;
 	}	
 }
