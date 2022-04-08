7ca3e73b392f3715bbd7457f2f2988d7cc79e66a
Shakshi Singhai <ssingha2@engr-ras-204.eos.ncsu.edu>
2022-04-08 17:06:33 -0400

    Updated App.java and remove delete from Edit/Publish

--- Main/src/operations/App.java
+++ Main/src/operations/App.java
@@ -9,8 +9,8 @@ import operations.Report;
 
 public class App {
     //database parameters Replace with your parameters
-    static String user = "pthosan";
-    static String password = "200401606";
+    static String user = "ssingha2";
+    static String password = "200370963";
 
     public static void main(String[] args) throws Exception {
         
@@ -50,78 +50,87 @@ public class App {
                                 else
                                     con.rollback();
                                 break; //call createPublication method in Production.java file
-                    case "2" : System.out.println("Unimplemented");
-                            break;
-                    case "3" : System.out.println("Unimplemented");
+                    case "2" : con.setSavepoint("beforeEditorInsert");
+                                if(Edit_Publish.newEditor(con,inputReader))
+                                    con.commit();
+                                else
+                                    con.rollback();
                                 break;
-                    case "4" : con.setSavepoint("editorviewPublication");
+                    case "3" : System.out.println("Unimplemented");
+                            break;
+                    case "4" : con.setSavepoint("beforeupdateEditor");
+                                if(Edit_Publish.updateEditor(con,inputReader))
+                                    con.commit();
+                                else
+                                    con.rollback();
+                                break;
+                    
+                    case "5" : con.setSavepoint("AfterEditorInsert");
+                                if(Edit_Publish.assignsnewAuthors(con,inputReader))
+                                    con.commit();
+                                else
+                                    con.rollback();
+                                break;
+                    case "6" : con.setSavepoint("editorviewPublication");
                                 if(Edit_Publish.editorviewPublication(con,inputReader))
                                     con.commit();
                                 else
                                     con.rollback();
                                 
                                 break;
-                    case "5" : System.out.println("Unimplemented");
+                    case "7" : System.out.println("Unimplemented");
                                 break;
-                    case "6" : System.out.println("Unimplemented");
+                    case "8" : System.out.println("Unimplemented");
                                 break;
-                    case "7" : con.setSavepoint("beforeProdInsert");
+                    case "9" : System.out.println("Unimplemented");
+                                break;
+                    case "10" : System.out.println("Unimplemented");
+                                break;
+                    case "11" : con.setSavepoint("beforeProdInsert");
                                 if(Production.newPublication(con,inputReader))
                                     con.commit();
                                 else
                                     con.rollback();
                                 break; //call createPublication method in Production.java file
-                    case "8" : con.setSavepoint("beforeProdUpdate");
+                    case "12" : con.setSavepoint("beforeProdUpdate");
                                 if(Production.updateProd(con,inputReader))
                                     con.commit();
                                 else
                                     con.rollback();
                                 break;
-                    case "9" : con.setSavepoint("beforeProdDelete");
+                    case "13" : con.setSavepoint("beforeProdDelete");
                                 if(Production.deleteProd(con,inputReader))
                                     con.commit();
                                 else
                                     con.rollback();
                                 break;
-                    case "10" : System.out.println("Unimplemented");
+                    case "14" : System.out.println("Unimplemented");
                                 break;
-                    case "11" : System.out.println("Unimplemented");
+                    case "15" : System.out.println("Unimplemented");
                                 break;
-                    case "12" : System.out.println("Unimplemented");
-                                break;
-                    case "13" : System.out.println("Unimplemented");
-                                break;
-                    case "14" : Savepoint distInsert=con.setSavepoint("beforeDistInsert");
-                                if(Distribution.newDist(con,inputReader))
-                                    con.commit();
-                                else
-                                    con.rollback(distInsert);
-                                break;
-                    case "15" : Savepoint distUpdate=con.setSavepoint("beforeDistupdate");
-                                if(Distribution.updateDist(con,inputReader))
-                                    con.commit();
-                                else
-                                    con.rollback(distUpdate);
-                                break;
-                    case "16" : Savepoint deldist=con.setSavepoint("beforeDistInsert");
-                                if(Distribution.deleteDist(con,inputReader))
-                                    con.commit();
-                                else
-                                    con.rollback(deldist);
+                    case "16" : System.out.println("Unimplemented");
                                 break;
                     case "17" : System.out.println("Unimplemented");
                                 break;
                     case "18" : System.out.println("Unimplemented");
                                 break;
-                    case "19" : System.out.println("Unimplemented");
+                    case "19" : Savepoint distInsert=con.setSavepoint("beforeDistInsert");
+                                if(Distribution.newDist(con,inputReader))
+                                    con.commit();
+                                else
+                                    con.rollback(distInsert);
                                 break;
-                    case "20" : con.setSavepoint("beforeDistInsert");
-			                    if(Report.monthlyPublication(con,inputReader))
-			                        con.commit();
-			                    else
-			                        con.rollback();
-			                    break;	
-                    case "21" : System.out.println("Unimplemented");
+                    case "20" : Savepoint distUpdate=con.setSavepoint("beforeDistupdate");
+                                if(Distribution.updateDist(con,inputReader))
+                                    con.commit();
+                                else
+                                    con.rollback(distUpdate);
+                                break;
+                    case "21" : Savepoint deldist=con.setSavepoint("beforeDistInsert");
+                                if(Distribution.deleteDist(con,inputReader))
+                                    con.commit();
+                                else
+                                    con.rollback(deldist);
                                 break;
                     case "22" : System.out.println("Unimplemented");
                                 break;
@@ -129,42 +138,23 @@ public class App {
                                 break;
                     case "24" : System.out.println("Unimplemented");
                                 break;
-                    case "25" : System.out.println("Unimplemented");
+                    case "25" : con.setSavepoint("beforeDistInsert");
+			                    if(Report.monthlyPublication(con,inputReader))
+			                        con.commit();
+			                    else
+			                        con.rollback();
+			                    break;	
+                    case "26" : System.out.println("Unimplemented");
                                 break;
-                    case "26" : Statement st=con.createStatement();
-                            ResultSet rs=st.executeQuery("Select * from Publication");
-                            while(rs.next()){
-                                System.out.println(rs.getInt(1));
-                            }
-                            break;
-        
-                    
-                    case "27" : con.setSavepoint("beforeEditorInsert");
-                                if(Edit_Publish.newEditor(con,inputReader))
-                                    con.commit();
-                                else
-                                    con.rollback();
+                    case "27" : System.out.println("Unimplemented");
                                 break;
-                    
-                    case "28" : con.setSavepoint("AfterEditorInsert");
-                                if(Edit_Publish.assignsnewAuthors(con,inputReader))
-                                    con.commit();
-                                else
-                                    con.rollback();
+                    case "28" : System.out.println("Unimplemented");
                                 break;
-                    case "29" :con.setSavepoint("beforeupdateEditor");
-                                if(Edit_Publish.updateEditor(con,inputReader))
-                                    con.commit();
-                                else
-                                    con.rollback();
+                    case "29" : System.out.println("Unimplemented");
                                 break;
-                    case "30" :con.setSavepoint("beforedeleteEditor");
-                                if(Edit_Publish.deleteEditor(con,inputReader))
-                                    con.commit();
-                                else
-                                    con.rollback();
+                    case "30" : System.out.println("Unimplemented");
                                 break;
-                    
+                   
 
                     default:  System.out.println("Invalid Input");
                                 break;
@@ -195,50 +185,48 @@ public class App {
         //instructions for editing/publishing
         System.out.println("\tEditing/Publishing:");
         System.out.println("\t\t [1] - Enter basic information on a new publication ");
-        System.out.println("\t\t [2] - Update Publication Information ");
-        System.out.println("\t\t [3] - Assign Editor(s) to Publication");
-        System.out.println("\t\t [4] - Let each editor view the information on the publications he/she is responsible for");        
-        System.out.println("\t\t [5] - Add Articles/Chapters to Publications ");
-        System.out.println("\t\t [6] - Delete Articles/Chapters to Publications ");
+        System.out.println("\t\t [2] - Enter basic information of a Editor");
+        System.out.println("\t\t [3] - Update Publication Information ");
+        System.out.println("\t\t [4] - Update Editor Information ");
+        System.out.println("\t\t [5] - Assign Editor(s) to Publication");
+        System.out.println("\t\t [6] - Let each editor view the information on the publications he/she is responsible for");        
+        System.out.println("\t\t [7] - Add Articles to Publications ");
+        System.out.println("\t\t [8] - Add Chapters to Publications ");
+        System.out.println("\t\t [9] - Delete Articles to Publications ");
+        System.out.println("\t\t [10] - Delete Chapters to Publications ");
         System.out.println();
 
         //instructions for production
         System.out.println("\tProduction:");
-        System.out.println("\t\t [7] - Enter a new book edition or new issue of a publication");
-        System.out.println("\t\t [8] - Update a book edition or publication issue.");
-        System.out.println("\t\t [9] - Delete a book edition or publication issue.");
-        System.out.println("\t\t [10] - Enter/Update an article or chapter OR Enter/Update text of an article");
-        System.out.println("\t\t [11] - Find books and articles by topic, date, author's name");
-        System.out.println("\t\t [12] - Enter payment for author or editor");
-        System.out.println("\t\t [13] - Keep track of when each payment was claimed by its addressee");
+        System.out.println("\t\t [11] - Enter a new book edition or new issue of a publication");
+        System.out.println("\t\t [12] - Update a book edition or publication issue.");
+        System.out.println("\t\t [13] - Delete a book edition or publication issue.");
+        System.out.println("\t\t [14] - Enter/Update an article or chapter");
+        System.out.println("\t\t [15] - Enter/Update text of an article");
+        System.out.println("\t\t [16] - Find books and articles by topic, date, author's name");
+        System.out.println("\t\t [17] - Enter payment for author or editor");
+        System.out.println("\t\t [18] - Keep track of when each payment was claimed by its addressee");
         System.out.println();
     
         //instructions for distribution
         System.out.println("\tDistribution");
-        System.out.println("\t\t [14] - Enter new distributor");
-        System.out.println("\t\t [15] - Update distributor information");
-        System.out.println("\t\t [16] - Delete a distributor. ");
-        System.out.println("\t\t [17] - Input orders from distributors, for a book edition or an issue of a publication per distributor, for a certain date.");
-        System.out.println("\t\t [18] - Bill distributor for an order");
-        System.out.println("\t\t [19] - Change outstanding balance of a distributor on receipt of a payment.");
+        System.out.println("\t\t [19] - Enter new distributor");
+        System.out.println("\t\t [20] - Update distributor information");
+        System.out.println("\t\t [21] - Delete a distributor. ");
+        System.out.println("\t\t [22] - Input orders from distributors, for a book edition or an issue of a publication per distributor, for a certain date.");
+        System.out.println("\t\t [23] - Bill distributor for an order");
+        System.out.println("\t\t [24] - Change outstanding balance of a distributor on receipt of a payment.");
         System.out.println();   
         
         //instructions for reports
         System.out.println("\tReports");
-        System.out.println("\t\t [20] - Generate montly reports: number and total price of copies of each publication bought per distributor per month");
-        System.out.println("\t\t [21] - Generate montly reports: total revenue of the publishing house");
-        System.out.println("\t\t [22] - Generate montly reports: total expenses (i.e., shipping costs and salaries)");
-        System.out.println("\t\t [23] - Calculate the total current number of distributors");
-        System.out.println("\t\t [24] - Calculate total revenue (since inception) per city, per distributor, and per location");
-        System.out.println("\t\t [25] - Calculate total payments to the editors and authors, per time period and per work type (book authorship, article authorship, or editorial work)");
+        System.out.println("\t\t [25] - Generate montly reports: number and total price of copies of each publication bought per distributor per month");
+        System.out.println("\t\t [26] - Generate montly reports: total revenue of the publishing house");
+        System.out.println("\t\t [27] - Generate montly reports: total expenses (i.e., shipping costs and salaries)");
+        System.out.println("\t\t [28] - Calculate the total current number of distributors");
+        System.out.println("\t\t [29] - Calculate total revenue (since inception) per city, per distributor, and per location");
+        System.out.println("\t\t [30] - Calculate total payments to the editors and authors, per time period and per work type (book authorship, article authorship, or editorial work)");
         System.out.println();
-        
-        //instructions for editor
-        System.out.println("\tEditor");
-        System.out.println("\t\t [27] - Enter basic information of a Editor");
-        System.out.println("\t\t [28] - Assign Editor to Publication ");
-        System.out.println("\t\t [29] - Update Editor ");
-        System.out.println("\t\t [30] - Delete Editor ");
         
 
     }
